a6e91a92138ea6130943e38cb7a3e30f628c9b82
Alex Heusingfeld <aheusingfeld@firstpoint.de>
2011-09-24 23:44:04 +0200

    added setup script for mysql database

---
+++ doc/photooapp-mysql-setup.sql
@@ -0,0 +1,14 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.1.44)
+# Datenbank: photooapp
+# Erstellungsdauer: 2011-09-24 21:31:39 +0000
+# ************************************************************
+
+CREATE DATABASE photooapp DEFAULT CHARSET=utf8;
+GRANT ALL ON photooapp.* TO photooapp@localhost IDENTIFIED BY 'pho!00App';
